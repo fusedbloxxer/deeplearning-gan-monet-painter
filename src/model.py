2e028e7f35c292f17af9d42147a93232a899754f
fusedbloxxer <voidwarden@yahoo.com>
2022-04-11 12:49:40 +0300

    GAN tweaking

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ import torch
 
 
 class Generator(nn.Module):
-    def __init__(self, noise: int, features: int, channels: int, bias: bool, device: torch.device):
+    def __init__(self, noise: int, features: int, channels: int, bias: bool, device: torch.device, activ_fun: nn.Module):
         super().__init__()
 
         # Parameters
@@ -16,23 +16,52 @@ class Generator(nn.Module):
         self.network = nn.Sequential()
 
         # Add multiple conv -> batch -> relu layers
-        for layer in range(5):
+        for layer in range(6):
             # Create intermediary layer
             convt = nn.Sequential(
-                nn.ConvTranspose2d(
+                nn.Upsample(scale_factor=2, mode='bilinear'),
+                nn.Conv2d(
                     in_channels=self.features // (2 ** layer),
-                    out_channels=self.features // (2 ** (layer + 1)),
-                    kernel_size=(4, 4),
-                    stride=2,
+                    out_channels=self.features // (2 ** layer),
+                    kernel_size=(3, 3),
+                    stride=1,
                     padding=1,
                     bias=bias,
                     device=device
                 ),
+                activ_fun,
+                nn.BatchNorm2d(
+                    num_features=self.features // (2 ** layer),
+                    device=device
+                ),
+                nn.Conv2d(
+                    in_channels=self.features // (2 ** layer),
+                    out_channels=self.features // (2 ** layer),
+                    kernel_size=(3, 3),
+                    stride=1,
+                    padding=1,
+                    bias=bias,
+                    device=device
+                ),
+                activ_fun,
+                nn.BatchNorm2d(
+                    num_features=self.features // (2 ** layer),
+                    device=device
+                ),
+                nn.Conv2d(
+                    in_channels=self.features // (2 ** layer),
+                    out_channels=self.features // (2 ** (layer + 1)),
+                    kernel_size=(3, 3),
+                    stride=1,
+                    padding=1,
+                    bias=bias,
+                    device=device
+                ),
+                activ_fun,
                 nn.BatchNorm2d(
                     num_features=self.features // (2 ** (layer + 1)),
                     device=device
                 ),
-                nn.ReLU(True),
             )
 
             # Append layer to network
@@ -40,11 +69,11 @@ class Generator(nn.Module):
 
         # Add final layer without BatchNorm and use Tanh
         self.network.append(nn.Sequential(
-            nn.ConvTranspose2d(
-                in_channels=self.features // (2 ** 5),
+            nn.Conv2d(
+                in_channels=self.features // (2 ** 6),
                 out_channels=channels,
-                kernel_size=(4, 4),
-                stride=2,
+                kernel_size=(3, 3),
+                stride=1,
                 padding=1,
                 bias=bias,
                 device=device
@@ -65,7 +94,7 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, in_chan: int, features: int, channels: int, device: torch.device, bias: bool):
+    def __init__(self, in_chan: int, features: int, channels: int, device: torch.device, bias: bool, activ_fun: nn.Module):
         super().__init__()
 
         # Create the network using conv layers
@@ -76,13 +105,13 @@ class Discriminator(nn.Module):
             nn.Conv2d(
                 in_channels=in_chan,
                 out_channels=features,
-                kernel_size=(4, 4),
+                kernel_size=(3, 3),
                 device=device,
                 bias=bias,
                 stride=2,
                 padding=1,
             ),
-            nn.LeakyReLU(0.2, inplace=True),
+            activ_fun,
         ))
 
         # Add each internediary conv layer
@@ -92,7 +121,7 @@ class Discriminator(nn.Module):
                 nn.Conv2d(
                     in_channels=features * (2 ** layer),
                     out_channels=features * (2 ** (layer + 1)),
-                    kernel_size=(4, 4),
+                    kernel_size=(3, 3),
                     device=device,
                     bias=bias,
                     stride=2,
@@ -102,7 +131,7 @@ class Discriminator(nn.Module):
                     features * (2 ** (layer + 1)),
                     device=device
                 ),
-                nn.LeakyReLU(0.2, inplace=True),
+                activ_fun,
             )
 
             # Append layer to network
